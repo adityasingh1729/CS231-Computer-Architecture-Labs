d58b69117f4ed208a89eaa542eae286fbd4e1fd4
adityasingh1729 <adityasingh4024@gmail.com>
2024-03-15 01:22:30 +0530

    Lab3 more parts added

---
+++ Lab3/Lab3Part3/22B1844/my_LINCOMB_ROW.asm
@@ -0,0 +1,97 @@
+        section .text
+        global lin_comb
+
+lin_comb:
+        push rdi                       ; pointer to mat1
+        push rsi                       ; scaler 1
+        push rdx                       ; pointer to mat2
+        push rcx                       ; scaler 2
+        push r8                        ; pointer to mat3
+        push r9                        ; number of rows/ columns of the matrix (n)
+        push r11
+        push r12
+        push r13
+        push r14
+        push r15
+
+; ; 0-indexing on all matrices
+; ; mat1[j][i] = rdi+(r9*j+i)*8
+; ; GOAL - Perform matrix linear combination of mat1, mat2 and save result in mat3
+; ; Note : mat1, mat2 and mat3 here are not the same as one given in problem statement.
+; ; They are just placeholders for any three matrices using this functionality.
+
+; ; TODO - Fill your code here performing the matrix linear combination in the following order
+; ; for(int i = 0; i < n; i++){for(int j = 0; j < n; j++){mat3[i][j] = s1*mat1[i][j] + s2*mat2[i][j];}}
+
+        ;push rcx  ;; Just for safety
+        ;push rsi
+
+        ;; storing i in r11
+        ;; storing j in r12
+
+        mov r11, 0
+        mov r12, 0
+
+        outer_loop:
+                cmp r11, r9
+                jge exit
+                mov r12, 0
+                jmp inner_loop
+
+
+        inner_loop:
+                cmp r12, r9
+                jge exit_inner_loop
+                push r11
+                imul r11, r9
+                add r11, r12
+                imul r11, 8
+                add r11, rdi   ;; r11 stores the address of mat1[j][i]
+                mov r13, [r11] ;; r13 stores value of mat1[j][i]
+                pop r11
+
+                push r11
+                imul r11, r9
+                add r11, r12
+                imul r11, 8
+                add r11, rdx   ;; r11 stores the address of mat2[j][i]
+                mov r14, [r11] ;; r14 stores value of mat2[j][i]
+                pop r11
+
+                imul r13, rsi  ;; r13 <-- s1*mat1[j][i]
+                imul r14, rcx  ;; r14 <-- s2*mat2[j][i]
+
+                add r13, r14   ;; r13 <-- s1*mat1[j][i] + s2*mat2[j][i]
+
+                push r11
+                imul r11, r9
+                add r11, r12
+                imul r11, 8
+                add r11, r8     ;; r11 stores the address of mat3[j][i]
+                mov [r11], r13  ;; storing calculated value in mat3 
+                pop r11
+
+                add r12, 1
+                jmp inner_loop
+
+        exit_inner_loop:
+                add r11, 1
+                jmp outer_loop
+
+        exit:
+
+
+; ; End of code to be filled
+
+        pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop r11
+        pop r9
+        pop r8
+        pop rcx
+        pop rdx
+        pop rsi
+        pop rdi
+        ret
---
+++ Lab3/Lab3Part3/22B1844/my_SUM_COL.asm
@@ -0,0 +1,74 @@
+        section .text
+        global  alt_sum
+
+alt_sum:
+        push rdi                       ; pointer to mat
+        push rdx                       ; number of rows/ columns of the matrix (n)
+        push r11
+        push r12
+        push r13
+        push r14
+        push r15
+
+; ; 0-indexing on all matrices
+; ; mat1[j][i] = rdi+(r9*j+i)*8
+; ; GOAL - Perform matrix alternate summation of elements in matrix and return the sum
+
+; ; TODO - Fill your code here performing the matrix alternate summation in the following order
+; ; for(int i = 0; i < n; i++){for(int j = 0; j < n; j++){sum += (-1)^(i+j)*mat[j][i];}}
+
+        ;; storing i in r11
+        ;; storing j in r12
+        ;; storing the sum in r14
+        ;; storing (-1)^(i+j) in r15
+
+        mov r11, 0
+        mov r12, 0
+        mov r14, 0
+        mov r15, 1  ;; i = j = 0
+                    ;; value of r15 has to change only when value of i or j changes
+                    ;; so we change r15 when we do i++ or j++
+                    ;; because it is equivalent to multiplying r15 by -1
+
+        outer_loop:
+                cmp r11, r9
+                jge exit
+                mov r12, 0
+                jmp inner_loop
+
+
+        inner_loop:
+                cmp r12, r9
+                jge exit_inner_loop
+                push r12
+                imul r12, r9
+                add r12, r11
+                imul r12, 8
+                add r12, rdi   ;; r12 stores the address of mat[j][i]
+                mov r13, [r12] ;; r13 stores value of mat[j][i]
+                pop r12
+                imul r13, r15  ;; r13 <-- (-1)^(i+j)*mat[j][i]
+                add r14, r13
+
+                add r12, 1   ;; j++
+                imul r15, -1 ;; modifying (-1)^(i + j)
+                jmp inner_loop
+
+        exit_inner_loop:
+                add r11, 1   ;; i++
+                imul r15, -1 ;; modifying (-1)^(i + j)
+                jmp outer_loop
+
+        exit:
+                mov rax, r14
+
+; ; End of code to be filled
+
+        pop r15
+        pop r14
+        pop r13
+        pop r12
+        pop r11
+        pop rdx
+        pop rdi
+        ret
